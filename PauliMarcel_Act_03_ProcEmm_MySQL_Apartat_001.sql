c26055ec132f5115c86796139661646f84f4a173
marcelpauli <pauligol7@gmail.com>
2021-02-12 17:44:24 +0100

    PauliMarcel_Act_03_ProcEmm_MYSQL

--- PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_001.sql
+++ PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_001.sql
@@ -1,7 +1,7 @@
 
 DELIMITER //
-USE videoclub//
-DROP PROCEDURE IF EXISTS Act_03_Apartat_001//
+USE videoclub //
+DROP PROCEDURE IF EXISTS Act_03_Apartat_001 //
 
 CREATE PROCEDURE Act_03_Apartat_001(
      IN  pe_codi smallint
@@ -14,7 +14,8 @@ CREATE PROCEDURE Act_03_Apartat_001(
      INNER JOIN ACTORS_PELLICULES ACT_PELI
         INNER JOIN ACTORS ACT
   ON  PELIS.id_peli=ACT_PELI.id_peli 
-      AND  ACT_PELI.id_actor=ACT.id_actor
-  WHERE PELIS.id_peli=pe_codi;
+      AND  ACT_PELI.id_actor = ACT.id_actor
+  WHERE PELIS.id_peli = pe_codi;
  END //
+
 DELIMITER ;
