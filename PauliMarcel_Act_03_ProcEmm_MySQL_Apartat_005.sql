8604b3a21fbb5bb8348b6b05f16a007abe250777
marcelpauli <pauligol7@gmail.com>
2021-02-18 16:29:05 +0100

    PauliMarcel_Act_03_ProcEmm_MYSQL

---
+++ PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_005.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+USE videoclub //
+DROP PROCEDURE IF EXISTS Act_03_Apartat_005 //
+CREATE PROCEDURE Act_03_Apartat_005(
+  IN c_peli smallint
+)
+  BEGIN
+    DECLARE nomactor varchar(30);
+    DECLARE edatactor smallint unsigned;
+    SELECT  nom_actor, YEAR(CURDATE())-anynaix_actor
+      INTO @nomactor, @edatactor
+    FROM  ACTORS
+    INNER  JOIN ACTORS_PELLICULES
+     ON  ACTORS.id_actor = ACTORS_PELLICULES.id_actor
+    WHERE  principal = 1 AND ACTORS_PELLICULES.id_peli = c_peli;      
+  END //
+DELIMITER ;
+CALL Act_03_Apartat_004(2);
+SELECT @actorPrincipal;
