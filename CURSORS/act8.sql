3b17a44608b1ae5407c13ca95f2bce43417fe71f
marcelpauli <pauligol7@gmail.com>
2021-04-07 17:48:05 +0200

    cursors

---
+++ CURSORS/act8.sql
@@ -0,0 +1,34 @@
+use videoclub;
+drop procedure if exists act8;
+delimiter / / create procedure act8() begin
+declare recaptat bigint default 0;
+declare pressu bigint default 0;
+declare titol varchar(30);
+declare rendibilitat varchar(15);
+declare final int default false;
+declare elcursor cursor for
+select titulo,
+    recaudacio,
+    pressupost
+from peliculas;
+declare continue handler for not found
+set final = 1;
+open elcursor;
+elbucle :loop fetch elcursor into titol,
+recaptat,
+pressu;
+if final = 1 then leave elbucle;
+end if;
+if (pressu > recaptat) then
+set rendibilitat = "Deficitari";
+elseif (pressu * 2 > recaptat) then
+set rendibilitat = "Suficient";
+else
+set rendibilitat = "Bona";
+end if;
+select titol,
+    rendibilitat;
+end loop elbucle;
+close elcursor;
+end / / delimiter;
+call act8();
