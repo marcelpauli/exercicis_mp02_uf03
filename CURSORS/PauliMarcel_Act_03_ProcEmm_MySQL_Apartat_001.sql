1423fd51c90f2be71bf7dfcda20f7c4ae15f33be
marcelpauli <pauligol7@gmail.com>
2021-04-09 16:15:33 +0200

    Exercicis Basededades

---
+++ CURSORS/PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_001.sql
@@ -0,0 +1,23 @@
+
+DELIMITER //
+USE videoclub //
+DROP PROCEDURE IF EXISTS Act_03_Apartat_001 //
+
+CREATE PROCEDURE Act_03_Apartat_001(
+     IN  pe_codi smallint
+)
+  BEGIN
+  SELECT PELIS.titol_peli Titol,
+         ACT_PELI.papel Paper,
+         ACT.nom_actor Actor
+  FROM   PELLICULES PELIS
+     INNER JOIN ACTORS_PELLICULES ACT_PELI
+        INNER JOIN ACTORS ACT
+  ON  PELIS.id_peli=ACT_PELI.id_peli 
+      AND  ACT_PELI.id_actor = ACT.id_actor
+  WHERE PELIS.id_peli = pe_codi;
+ END //
+
+DELIMITER ;
+
+CALL Act_03_Apartat_001();
---
+++ CURSORS/PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_004.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+USE videoclub //
+DROP PROCEDURE IF EXISTS Act_03_Apartat_004 //
+CREATE PROCEDURE Act_03_Apartat_004(
+  IN c_peli smallint
+)
+  BEGIN
+    DECLARE actorPrincipal smallint unsigned;
+    SELECT  id_actor
+      INTO @actorPrincipal
+    FROM ACTORS_PELLICULES
+    WHERE  principal = 1 AND id_peli = c_peli;      
+  END //
+DELIMITER ;
+CALL Act_03_Apartat_004(2);
+SELECT @actorPrincipal;
