ab53d7c48af6712c45f26a11143183a5bd98ccd0
marcelpauli <pauligol7@gmail.com>
2021-05-04 18:02:32 +0200

    BaseDeDades

--- CURSORS/PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_009.sql
+++ CURSORS/PauliMarcel_Act_03_ProcEmm_MySQL_Apartat_009.sql
@@ -1,25 +1,44 @@
 USE videoclub;
+DROP PROCEDURE IF EXISTS act9;
 DELIMITER //
-DROP PROCEDURE IF EXISTS PardoJoan_Act_03_ProcEmm_MySQL_Apartat_010//
-CREATE PROCEDURE PardoJoan_Act_03_ProcEmm_MySQL_Apartat_010(
- IN  pi_actor       smallint)
-  BEGIN
-   DECLARE pa_nomActor       varchar(30);
-   DECLARE pa_edatActor      smallint;
-   DECLARE pa_sexeActor      varchar(6);
-   DECLARE pa_paper          varchar(50);
-SELECT nom_actor, year(curdate())-anynaix_actor, sexe_actor
-   INTO pa_nomActor, pa_edatActor, pa_sexeActor
-FROM   ACTORS
-WHERE  id_actor=pi_actor; 
+CREATE PROCEDURE act9()
+BEGIN
+   DECLARE edat bigint;
+   DECLARE nom varchar(30);
+   DECLARE sexe varchar(6);
+   DECLARE final int default false;
+   DECLARE tipusDePaper varchar(6);
+   DECLARE elcursor cursor FOR
+      SELECT ACTORS.sexe_actor, YEAR(CURDATE())-ACTORS.anynaix_actor, ACTORS.nom_actor
+   FROM ACTORS;
 
-IF pa_sexeActor = "home" THEN
-   IF pa_edatActor<15 THEN
-      SET pa_paper = "de nen";
-   ELSEIF  pa_edatActor<25 THEN 
-         SET pa_paper = "dona gran";
-     END IF;
-    SELECT concat("L'actriu ", pa_nomActor, " té ", pa_edatActor , " anys i pot fer de ", pa_paper, ".") AS Frase;
-END IF;
-  END //
+   DECLARE CONTINUE handler for NOT found SET final = 1;
+   OPEN elcursor;
+   elbucle:LOOP
+      FETCH elcursor INTO sexe, edat, nom;
+
+      IF final = 1 THEN
+         leave elbucle;
+      END IF;
+
+      IF edat < 15 THEN
+    SET tipusDePaper ="nen";
+    ELSEIF edat > 15 AND edat < 25 THEN
+    SET tipusDePaper ="jove";
+    ELSEIF edat > 26 AND edat < 40 THEN
+    SET tipusDePaper ="adult";
+    ELSEIF edat > 41 AND edat < 60 THEN
+    SET tipusDePaper ="madur";
+    ELSEIF edat > 61 THEN
+    SET tipusDePaper ="gran";
+    END IF;
+
+    SELECT nom, sexe, edat, tipusDePaper;
+
+   END LOOP elbucle; 
+   CLOSE elcursor;
+END//
+
 DELIMITER ;
+
+call act9();
